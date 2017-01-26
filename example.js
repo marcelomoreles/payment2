b67cc4950399cb5bd02dcc20fc22089e1febaab8
Jorge Rafael <jorgerafael@uxer.com.br>
2017-01-26 03:50:18 -0200

    Encapsulando os arquivos.

--- example.js
+++ example.js
@@ -1,35 +1,56 @@
-const payment = require('payment2')
+const payment = require('payment2');
 
-const pay = () => {
-	const settings = {
-    	email: 'lukasc4l@gmail.com'
-		, token: 'AOSDIAU8979234'
-		, ambient: 'production'
-		, logging: false
-	}
-	const callback = (response) => console.log('Session ID:', response)
-	const initialize = () => payment.initialize(payment.credentials, callback)
-	payment.settings(settings, initialize)
-}
+(function() {
+  'use strict';
 
-const pay2 = () => {
-	const settings = () => {
-		payment.settings.email: 'lukasc4l@gmail.com'
-		payment.settings.token: 'AOSDIAU8979234'
-		payment.settings.ambient: 'production'
-		payment.settings.logging: false
-	}
-	const callback = (response) => console.log('Session ID:', response)
-	payment.initialize(settings, callback)
-}
 
-const pay3 = () => {
-	const settings = {
-		  email: 'lukasc4l@gmail.com'
-		, token: 'AOSDIAU8979234'
-		, ambient: 'production'
-		, logging: false
-	}
-	const callback = (response) => console.log('Session ID:', response)
-	payment.initialize(settings, callback)
-}
+  const pay = () => {
+  	const settings = {
+      	email: 'lukasc4l@gmail.com'
+  		, token: 'AOSDIAU8979234'
+  		, ambient: 'production'
+  		, logging: false
+  	}
+  	const callback = (response) => console.log('Session ID:', response)
+  	const initialize = () => payment.initialize(payment.credentials, callback)
+  	payment.settings(settings, initialize)
+  }
+
+
+}());
+
+
+(function() {
+  'use strict';
+
+  const pay2 = () => {
+  	const settings = () => {
+  		payment.settings.email: 'lukasc4l@gmail.com'
+  		payment.settings.token: 'AOSDIAU8979234'
+  		payment.settings.ambient: 'production'
+  		payment.settings.logging: false
+  	}
+  	const callback = (response) => console.log('Session ID:', response)
+  	payment.initialize(settings, callback)
+  }
+
+
+}());
+
+
+(function() {
+  'use strict';
+
+  const pay3 = () => {
+  	const settings = {
+  		  email: 'lukasc4l@gmail.com'
+  		, token: 'AOSDIAU8979234'
+  		, ambient: 'production'
+  		, logging: false
+  	}
+  	const callback = (response) => console.log('Session ID:', response)
+  	payment.initialize(settings, callback)
+  }
+
+
+}());

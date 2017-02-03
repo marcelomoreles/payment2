11c283c3076409a981bfa98b5d0f24915c31b859
Lukas Wilkeer <lukasc4l@gmail.com>
2017-02-02 22:42:48 -0200

    Exemplo de utilização

--- example.js
+++ example.js
@@ -3,44 +3,6 @@ const payment = require('payment2');
 (function() {
   'use strict';
 
-
-  const pay = () => {
-  	const settings = {
-      	email: 'lukasc4l@gmail.com'
-  		, token: 'AOSDIAU8979234'
-  		, ambient: 'production'
-  		, logging: false
-  	}
-  	const callback = (response) => console.log('Session ID:', response)
-  	const initialize = () => payment.initialize(payment.credentials, callback)
-  	payment.settings(settings, initialize)
-  }
-
-
-}());
-
-
-(function() {
-  'use strict';
-
-  const pay2 = () => {
-  	const settings = () => {
-  		payment.settings.email: 'lukasc4l@gmail.com'
-  		payment.settings.token: 'AOSDIAU8979234'
-  		payment.settings.ambient: 'production'
-  		payment.settings.logging: false
-  	}
-  	const callback = (response) => console.log('Session ID:', response)
-  	payment.initialize(settings, callback)
-  }
-
-
-}());
-
-
-(function() {
-  'use strict';
-
   const pay3 = () => {
   	const settings = {
   		  email: 'lukasc4l@gmail.com'
